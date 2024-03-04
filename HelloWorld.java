b321ed7c9a203236d2a6942a466176d31a765e6b
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2024-03-04 18:12:02 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        byte byteVar = 1;
+        System.out.println("Byte " + byteVar);
+        short shortVar = 200;
+        System.out.println("Short " + shortVar);
+        int intVar = 21742000;
+        System.out.println("Int " + intVar);
+        long longVar = 1462487958;
+        System.out.println("Long " + longVar);
+        float floatVar = 400.2F;
+        System.out.println("Float " + floatVar);
+        double doubleVar = 588858.99;
+        System.out.println("Double " + doubleVar);
+        char charVar = 'A';
+        System.out.println("Char " + charVar);
+        boolean booleanVar = true;
+        System.out.println("Boolean " + booleanVar);
+        var varChar = true;
+        System.out.println("var " + varChar);
+
+    }
+}
