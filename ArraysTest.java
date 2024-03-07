ce6a85ed8cf74ed9ef246d2f382e7719b163fd0e
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2024-03-07 18:39:48 +0100

    lekcje 12-14

--- ArraysTest.java
+++ ArraysTest.java
@@ -10,7 +10,7 @@ public class ArraysTest {
         array.add(2);
         array.add(3);
 
-        System.out.println(array.get(0));
+        // System.out.println(array.get(0));
 
         int[] array2 = new int[10];
         array2[0] = 1;
@@ -19,8 +19,30 @@ public class ArraysTest {
         array2[8] = 8;
 
         Arrays.sort(array2);
-        System.out.println(Arrays.toString(array2));
+        // System.out.println(Arrays.toString(array2));
+
+        String[] tablica = {"Mateusz", "Tadeusz", "Paulina"};
         
+        // for(int i = 0; i < tablica.length; i++){
+        //     System.err.println(tablica[i]);
+        // }
+
+        ArrayList<String> wyrazy = new ArrayList<>();
+        wyrazy.add(0, "Mateusz" );
+        wyrazy.add(1, "Tadeusz" );
+        wyrazy.add(2, "Paulina" );
+        wyrazy.addLast("Ostatni element");
+        wyrazy.addFirst("Pierwszy element");
+        // System.out.println(wyrazy.contains("Mateusz"));
+        wyrazy.forEach((element) -> {
+            System.err.println(element);
+        });
+
+        // for(int i = 0; i < wyrazy.size() ; i++){
+        //     System.out.println(wyrazy.get(i));
+            
+        // }
+
 
 
     //    int[] array1 = {1,2,3,4};
